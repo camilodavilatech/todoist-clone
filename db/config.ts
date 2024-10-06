3d65416617e648883040528325f81c1e36e1d394
fabianmc <fabianmctec@gmail.com>
2024-10-06 00:23:01 -0500

    add formulario nueva tarea bdatos y README

--- db/config.ts
+++ db/config.ts
@@ -1,4 +1,23 @@
-import { defineDb } from "astro:db";
+import { defineDb, defineTable, column } from "astro:db";
+
+const User = defineTable({
+  columns: {
+    idUser: column.number({ primaryKey: true }),
+    firstName: column.text(),
+    lastName: column.text(),
+    userAvatar: column.text(),
+    email: column.text(),
+  },
+});
+
+const Task = defineTable({
+  columns: {
+    idTask: column.number({ primaryKey: true }),
+    userId: column.number({ references: () => User.columns.idUser }),
+    description: column.text(),
+    dateTime: column.date(),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
